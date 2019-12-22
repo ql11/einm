9667351b5ad092808a3ee677866486cb92bb6b05
ql11 <656685187@qq.com>
2019-12-22 10:44:17 +0800

    增加输出文件夹指示，重设源磁场数据位置

--- MainProgram.m
+++ MainProgram.m
@@ -3,22 +3,22 @@
 start_time = datestr(now,' 日期yyyy-mm-dd 时间HH:MM:SS');
 disp([' 程序开始时间：【',start_time,'】']);
 
-fid=fopen('程序记录yyyy-mm-dd.txt','w');
+fid=fopen('程序记录日期yyyy-mm-dd.txt','w');
 fprintf(fid,['程序开始时间：【',start_time,'】','\n']);
-fid_1 = fopen('【数据记录】所有击中目标区域的电子.txt','w');
-fid_2 = fopen('【数据记录】无磁场时会打到目标区域的电子.txt','w');
-fid_3 = fopen('【数据记录】无磁场时会打到目标区域，加上磁场之后也会打到目标区域的电子.txt','w');
-fid_4 = fopen('【数据记录】无磁场时不会打到目标区域，而加上磁场之后会打到目标区域的电子.txt','w');
-fid_5 = fopen('【数据记录】无磁场时会打到目标区域，加上磁场之后不会打到目标区域的电子.txt','w');
-fid_6 = fopen('全部电子落点【正负2.50m】.txt','w');
-fid_7 = fopen('计算的电子个数.txt','w');
+fid_1 = fopen('output/【数据记录】所有击中目标区域的电子.txt','w');
+fid_2 = fopen('output/【数据记录】无磁场时会打到目标区域的电子.txt','w');
+fid_3 = fopen('output/【数据记录】无磁场时会打到目标区域，加上磁场之后也会打到目标区域的电子.txt','w');
+fid_4 = fopen('output/【数据记录】无磁场时不会打到目标区域，而加上磁场之后会打到目标区域的电子.txt','w');
+fid_5 = fopen('output/【数据记录】无磁场时会打到目标区域，加上磁场之后不会打到目标区域的电子.txt','w');
+fid_6 = fopen('output/【数据记录】全部电子落点【正负2.50m】.txt','w');
+fid_7 = fopen('output/【数据记录】计算的电子个数.txt','w');
 
 fclose(fid);
 fclose(fid_1);fclose(fid_2);fclose(fid_3);fclose(fid_4);fclose(fid_5);fclose(fid_6);fclose(fid_7);
 %% 第一步读取磁场数据*********
 disp('程序开始，读取磁场数据中……');
 tic
-%load('/public1/home/sc40009/QinLang/From office/20190107/0110_V.mat');
+load('/public1/home/sc40009/jobs/Ger20kev/121601.mat');
 disp('0110_V.mat读取完毕，开始设定参数……');
 toc
 
@@ -36,20 +36,20 @@ Total = lx*ly*lphi*lsita;%总数
 parfor k = 0:(Total - 1)
     [mx,my,mphi,msita] = floopsettings(k,lx,ly,lphi,lsita);
     P0 = [mx/1000,my/1000,0.01999];
-    fid=fopen('程序记录yyyy-mm-dd.txt','a');
-    fid_1 = fopen('【数据记录】所有击中目标区域的电子.txt','a');
-    fid_2 = fopen('【数据记录】无磁场时会打到目标区域的电子.txt','a');
-    fid_3 = fopen('【数据记录】无磁场时会打到目标区域，加上磁场之后也会打到目标区域的电子.txt','a');
-    fid_4 = fopen('【数据记录】无磁场时不会打到目标区域，而加上磁场之后会打到目标区域的电子.txt','a');
-    fid_5 = fopen('【数据记录】无磁场时会打到目标区域，加上磁场之后不会打到目标区域的电子.txt','a');
-    fid_6 = fopen('全部电子落点【正负2.50m】.txt','a');
+    fid=fopen('程序记录日期yyyy-mm-dd.txt','a');
+    fid_1 = fopen('output/【数据记录】所有击中目标区域的电子.txt','a');
+    fid_2 = fopen('output/【数据记录】无磁场时会打到目标区域的电子.txt','a');
+    fid_3 = fopen('output/【数据记录】无磁场时会打到目标区域，加上磁场之后也会打到目标区域的电子.txt','a');
+    fid_4 = fopen('output/【数据记录】无磁场时不会打到目标区域，而加上磁场之后会打到目标区域的电子.txt','a');
+    fid_5 = fopen('output/【数据记录】无磁场时会打到目标区域，加上磁场之后不会打到目标区域的电子.txt','a');
+    fid_6 = fopen('output/【数据记录】全部电子落点【正负2.50m】.txt','a');
     
     if original_position(P0)
         V0 = [v*cos(-((msita/angle)/180+90/180)*pi)*sin((mphi/angle)*pi), ...
             v*cos(-((msita/angle)/180+90/180)*pi)*cos((mphi/angle)*pi), ...
             v*sin(-((msita/angle)/180+90/180)*pi)];
         %******记数********
-        fid_7 = fopen('计算的电子个数.txt','a');
+        fid_7 = fopen('output/【数据记录】计算的电子个数.txt','a');
         nowtime = datestr(now,' 日期yyyy-mm-dd 时间HH:MM:SS');
         fprintf(fid_7,[nowtime,' ',' |',num2str(V0), ' |',num2str(P0),'|\n']);
         fclose(fid_7);
