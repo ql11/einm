2b03242171688bcdbb7637da5729265a1a8fa5b2
qinlang <656685187@qq.com>
2020-03-30 21:54:41 +0800

    循环函数

--- floopsettings.m
+++ floopsettings.m
@@ -1,15 +1,13 @@
-function [mx,my,mphi,msita] = floopsettings(k,lx,ly,lphi,lsita)
-    %将嵌套循环化为统一循环；排列次序以第三象限的次序为基准
-    if k < lx*ly*lphi*lsita
-
-        mx = 4*floor(k/(ly*lphi*lsita));
-        my = 4*floor(mod(k,(ly*lphi*lsita))/(lphi*lsita));
-        mphi = 10*floor(mod(mod(k,(ly*lphi*lsita)),(lphi*lsita))/lsita);
-        msita = 2*mod(mod(mod(k,(ly*lphi*lsita)),(lphi*lsita)),lsita) - 20;
-
-    else 
-        disp('循环超出');
-        mx = 0;my= 0;mphi= 0;msita= 0;
-    end
-    
+function [mx,my,mz,mphi,msita] = floopsettings(k,lx,ly);
+    %将嵌套循环化为统一循环
+    % k = 1~Total 
+    Special_z =[40.1 37.8 32.9 37.8	35.5 30.6 32.9 30.6	25.7];
+    Special_phi = [135	111.3 103.6 158.7 135.0 121.8 166.4 148.2 135];
+    Special_sita = [3.00 5.85 9.05 5.85	7.72 10.36 9.05	10.36 12.47];
+    my = mod(k-1,lx);
+    mx = ((k - 1) - my)/ly;
+    n = floor(9*k/lx/ly);
+    mz = Special_z(n);
+    mphi = Special_phi(n);
+    msita = Special_sita(n);
 end
