c33c63ef15dd95ff44b8246d74749b484f39066e
qinlang <656685187@qq.com>
2020-03-30 21:56:14 +0800

    修复

--- floopsettings.m
+++ floopsettings.m
@@ -1,4 +1,4 @@
-function [mx,my,mz,mphi,msita] = floopsettings(k,lx,ly);
+function [mx,my,mz,mphi,msita] = floopsettings(k,lx,ly)
     %将嵌套循环化为统一循环
     % k = 1~Total 
     Special_z =[40.1 37.8 32.9 37.8	35.5 30.6 32.9 30.6	25.7];
@@ -6,7 +6,7 @@ function [mx,my,mz,mphi,msita] = floopsettings(k,lx,ly);
     Special_sita = [3.00 5.85 9.05 5.85	7.72 10.36 9.05	10.36 12.47];
     my = mod(k-1,lx);
     mx = ((k - 1) - my)/ly;
-    n = floor(9*k/lx/ly);
+    n = floor(9*k/lx/ly + 1);
     mz = Special_z(n);
     mphi = Special_phi(n);
     msita = Special_sita(n);
