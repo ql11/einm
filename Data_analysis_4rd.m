c0f1c522925d08d57ac36ea3c117161e326d9d7c
qinlang <656685187@qq.com>
2020-04-01 18:28:49 +0800

    更新

--- Data_analysis_4rd.m
+++ Data_analysis_4rd.m
@@ -1,38 +1,68 @@
 %% step1
 
-path  = 'C:\Storage\data\einm\DATA_20200331';
+path  = 'C:\Storage\data\einm\DATA_20200401';
 
 lx = 142; % 0~141，每1mm一个点
 ly = 142; % 0~141，每1mm一个点
 Total = lx*ly;%总数
 %% 循环计算
 %%
-
-for E = [50 75 100 125 150 300 500 700 900 1000]
-    
+E_count = zeros(9,9); %每一行对应速度
+E0 = [50 75 100 125 150 300 500 700 1001];
+for i = 1:9
+    E = E0(i);
     %% 读取数据
     
     load([path,'/',num2str(E),'keV计算结果.mat']);
     load([path,'/',num2str(E),'keV电子落点.mat']);
     %电子落点图
-    %scatter(EMP(:,1),EMP(:,2),'.');
-    %figure
-    count = zeros(1,9);
-    for k = 1:9
+    figure
+    scatter(EMP(:,1),EMP(:,2),'.');
+    Data = zeros(Total,5);
+    parfor k = 1:Total
+        if EM1(k) == 1
+            Data(k,:) = floopsettings(k,lx,ly);
+        end
+    end
+    figure
+    scatter(Data(:,1),Data(:,2),'.');
 
-        k1 = (k - 1)*Total/9 + 1;
-        k2 = k*Total/9;
-        count(k) = sum(EM1(k1:k2));
+
+    % 分片
+    for k = 1:Total
+        my = mod(k-1,lx);
+        mx = ((k - 1) - my)/ly;  
+        n47 = (lx - 1)/3;
+        if mx <= n47
+            if my <= n47
+                n = 1;
+            elseif my <= 2*n47
+                n = 2;
+            else
+                n = 3;
+            end
+        elseif mx <= 2*n47
+            if my <= n47
+                n = 4;
+            elseif my <= 2*n47
+                n = 5;
+            else
+                n = 6;
+            end
+        else
+            if my <= n47
+                n = 7;
+            elseif my <= 2*n47
+                n = 8;
+            else
+                n = 9;
+            end
+        end
+        if EM1(k) == 1
+            E_count(i,n) = E_count(i,n) + 1;
+        end
 
     end
-    disp(num2str(count));
-    %Data = zeros(Total,5);
-    %parfor k = 1:Total
-        %if EM1(k) == 1
-            %Data(k,:) = floopsettings(k,lx,ly);
-        %end
-    %end
-    %scatter(Data(:,1),Data(:,2),'.');
-    %figure
+    disp([num2str(E),'击中个数:',num2str(E_count(i,:))]);
     
 end
